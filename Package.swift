f85573bc33a6677fd3ec96a1571514cd311ef420
Ryan Lovelett <ryan@lovelett.me>
2016-12-17 14:37:36 -0500

    Use Ogra SwiftPM repository

--- Package.swift
+++ Package.swift
@@ -3,14 +3,14 @@ import PackageDescription
 let package = Package(
     name: "langserver-swift",
     targets: [
-        Target(name: "Ogra"),
-        Target(name: "JSONRPC", dependencies: ["Ogra"]),
+        Target(name: "JSONRPC"),
         Target(name: "LanguageServerProtocol", dependencies: ["JSONRPC"]),
         Target(name: "LanguageServer", dependencies: ["LanguageServerProtocol", "JSONRPC"])
     ],
     dependencies: [
         .Package(url: "https://github.com/RLovelett/SourceKitten.git", majorVersion: 0),
         .Package(url: "https://github.com/RLovelett/Argo.git", majorVersion: 4),
+        .Package(url: "https://github.com/RLovelett/Ogra.git", majorVersion: 4),
         .Package(url: "https://github.com/thoughtbot/Curry.git", majorVersion: 3)
     ]
 )
