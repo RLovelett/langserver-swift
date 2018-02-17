c21ed51fcbdf3e798b9d7f74639d6cf19babafc3
Ryan Lovelett <ryan@lovelett.me>
2018-02-17 08:10:47 -0500

    Convert from waitForDataInBackgroundAndNotify to DispatchIO

--- Sources/LanguageServer/main.swift
+++ Sources/LanguageServer/main.swift
@@ -10,38 +10,39 @@ private let header: [String : String] = [
 let main = OperationQueue.main
 let stdin = FileHandle.standardInput
 var requests = RequestBuffer()
-stdin.waitForDataInBackgroundAndNotify()
 
-// When new data is available
-var dataAvailable : NSObjectProtocol!
-dataAvailable = NotificationCenter.default.addObserver(forName: .NSFileHandleDataAvailable, object: stdin, queue: main) { (notification) -> Void in
-    let buffer = stdin.availableData
+let global = DispatchQueue.global(qos: .background)
+let channel = DispatchIO(type: .stream, fileDescriptor: stdin.fileDescriptor, queue: global) { _ in }
 
-    guard !buffer.isEmpty else {
-        return stdin.waitForDataInBackgroundAndNotify()
+// The desire is to trigger the handler block as soon as there is any data in the channel.
+channel.setLimit(lowWater: 1)
+
+channel.read(offset: 0, length: Int.max, queue: .main) { (end, _, error) in
+  let buffer = stdin.availableData
+
+  guard !buffer.isEmpty else {
+    return
+  }
+
+  requests.append(buffer)
+
+  for requestBuffer in requests {
+    do {
+      let request = try Request(requestBuffer)
+      let response = handle(request)
+      /// If the request id is null then it is a notification and not a request
+      switch request {
+      case .request(_, _, _):
+        let toSend = response.data(header)
+        FileHandle.standardOutput.write(toSend)
+      default: ()
+      }
+    } catch let error as PredefinedError {
+      fatalError(error.description)
+    } catch {
+      fatalError("TODO: Better error handeling. \(error)")
     }
-
-    requests.append(buffer)
-
-    for requestBuffer in requests {
-        do {
-            let request = try Request(requestBuffer)
-            let response = handle(request)
-            /// If the request id is null then it is a notification and not a request
-            switch request {
-            case .request(_, _, _):
-                let toSend = response.data(header)
-                FileHandle.standardOutput.write(toSend)
-            default: ()
-            }
-        } catch let error as PredefinedError {
-            fatalError(error.description)
-        } catch {
-            fatalError("TODO: Better error handeling. \(error)")
-        }
-    }
-
-    return stdin.waitForDataInBackgroundAndNotify()
+  }
 }
 
 // Launch the task
